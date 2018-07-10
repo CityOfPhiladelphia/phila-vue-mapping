28911135320f4f684fed33f64645d16199caf6a8
andy.rothwell <rothwell.andy@gmail.com>
2018-07-10 15:34:47 -0400

    fixes bugs with links

--- src/main.js
+++ src/main.js
@@ -1,32 +1,32 @@
 import Map_ from './leaflet/Map.vue';
 import Control from './leaflet/Control.vue';
 import Geojson from './leaflet/Geojson.vue';
-import CircleMarker from '/leaflet/CircleMarker.vue';
-import ControlCorner from '/leaflet/ControlCorner.vue';
-import EsriTiledMapLayer from 'esri-leaflet/TiledMapLayer.vue';
-import EsriTiledOverlay from 'esri-leaflet/TiledOverlay.vue';
-import EsriDynamicMapLayer from 'esri-leaflet/DynamicMapLayer.vue';
-import EsriFeatureLayer from '/esri-leaflet/FeatureLayer.vue';
+import CircleMarker from './leaflet/CircleMarker.vue';
+import ControlCorner from './leaflet/ControlCorner.vue';
+import EsriTiledMapLayer from './esri-leaflet/TiledMapLayer.vue';
+import EsriTiledOverlay from './esri-leaflet/TiledOverlay.vue';
+import EsriDynamicMapLayer from './esri-leaflet/DynamicMapLayer.vue';
+import EsriFeatureLayer from './esri-leaflet/FeatureLayer.vue';
 
-import AddressInput from 'components/AddressInput.vue';
-import AddressCandidateList from 'components/AddressCandidateList.vue';
-import OpacitySlider from 'components/OpacitySlider.vue';
-import VectorMarker from 'components/VectorMarker.vue';
-import PngMarker from 'components/PngMarker.vue';
-import BasemapToggleControl from 'components/BasemapToggleControl.vue';
-import BasemapSelectControl from 'components/BasemapSelectControl.vue';
-import FullScreenMapToggleTab from 'components/FullScreenMapToggleTab.vue';
-import LocationControl from 'components/LocationControl.vue';
-import MeasureControl from 'components/MeasureControl.vue';
-import LegendControl from 'components/LegendControl.vue';
-import BasemapTooltip from 'components/BasemapTooltip.vue';
+import AddressInput from './components/AddressInput.vue';
+import AddressCandidateList from './components/AddressCandidateList.vue';
+import OpacitySlider from './components/OpacitySlider.vue';
+import VectorMarker from './components/VectorMarker.vue';
+import PngMarker from './components/PngMarker.vue';
+import BasemapToggleControl from './components/BasemapToggleControl.vue';
+import BasemapSelectControl from './components/BasemapSelectControl.vue';
+import FullScreenMapToggleTab from './components/FullScreenMapToggleTab.vue';
+import LocationControl from './components/LocationControl.vue';
+import MeasureControl from './components/MeasureControl.vue';
+import LegendControl from './components/LegendControl.vue';
+import BasemapTooltip from './components/BasemapTooltip.vue';
 
-import CyclomediaButton from '/cyclomedia/Button.vue';
-import CyclomediaRecordingCircle from '/cyclomedia/RecordingCircle.vue';
-import CyclomediaRecordingsClient from '/cyclomedia/recordings-client';
-import SvgViewConeMarker from '/cyclomedia/SvgViewConeMarker.vue';
+import CyclomediaButton from './cyclomedia/Button.vue';
+import CyclomediaRecordingCircle from './cyclomedia/RecordingCircle.vue';
+import CyclomediaRecordingsClient from './cyclomedia/recordings-client';
+import SvgViewConeMarker from './cyclomedia/SvgViewConeMarker.vue';
 
-import PictometryButton from '/pictometry/Button.vue';
+import PictometryButton from './pictometry/Button.vue';
 
 
 
