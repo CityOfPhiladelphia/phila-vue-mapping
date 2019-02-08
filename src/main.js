7652d0ee8c1afb960ce0b29ae64deb444df64f07
andy.rothwell <rothwell.andy@gmail.com>
2019-02-08 11:00:18 -0500

    makes all font-awesome libraries dependencies, adds lines to top of main.js to make it work

--- src/main.js
+++ src/main.js
@@ -1,3 +1,17 @@
+// Font Awesome Icons
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { dom } from '@fortawesome/fontawesome-svg-core'
+dom.watch() // This will kick of the initial replacement of i to svg tags and configure a MutationObserver
+
+import { faDotCircle } from '@fortawesome/pro-regular-svg-icons/faDotCircle';
+import { faSearch } from '@fortawesome/pro-solid-svg-icons/faSearch';
+import { faTimes } from '@fortawesome/pro-solid-svg-icons/faTimes';
+import { faMapMarkerAlt } from '@fortawesome/pro-solid-svg-icons/faMapMarkerAlt';
+import { faCaretLeft } from '@fortawesome/pro-solid-svg-icons/faCaretLeft';
+import { faCaretRight } from '@fortawesome/pro-solid-svg-icons/faCaretRight';
+library.add(faSearch, faTimes, faMapMarkerAlt, faDotCircle, faCaretLeft, faCaretRight);
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
+
 // Leaflet
 import Map_ from './leaflet/Map.vue';
 import Control from './leaflet/Control.vue';
