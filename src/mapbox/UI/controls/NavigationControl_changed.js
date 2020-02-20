8abd96040f5efc20c40d7e812eeb6153f5ee2532
andy.rothwell <rothwell.andy@gmail.com>
2020-02-20 16:13:39 -0500

    adds new components

---
+++ src/mapbox/UI/controls/NavigationControl_changed.js
@@ -0,0 +1,33 @@
+import controlMixin from "./controlMixin";
+
+export default {
+  name: "NavigationControl",
+  mixins: [controlMixin],
+
+  props: {
+    showCompass: {
+      type: Boolean,
+      default: true
+    },
+    showZoom: {
+      type: Boolean,
+      default: true
+    }
+  },
+
+  created() {
+    console.log('NavigationControl created, this.$store.state.map.map:', this.$store.state.map.map);
+    // this.control = new this.mapbox.NavigationControl(this.$props);
+    // this.$_addControl();
+  },
+  mounted() {
+    console.log('NavigationControl mounted, this.map:', this.map, 'this.$store.state.map:', this.$store.state.map);
+  },
+  methods: {
+    parentMounted(parent) {
+      console.log('NavigationControl parentMounted is running, this.$store.state.map:', this.$store.state.map);
+      this.control = new this.mapbox.NavigationControl(this.$props);
+      this.$_addControl();
+    }
+  }
+};
---
+++ src/mapbox/map/GlMap.vue
@@ -0,0 +1,132 @@
+<template>
+  <div class="mgl-map-wrapper">
+    <div v-once :id="container" ref="container" />
+    <slot v-if="initialized" />
+  </div>
+</template>
+
+<script>
+import withEvents from "../lib/withEvents";
+import mapEvents from "./events";
+import options from "./options";
+import withWatchers from "./mixins/withWatchers";
+import withPrivateMethods from "./mixins/withPrivateMethods";
+import withAsyncActions from "./mixins/withAsyncActions";
+
+export default {
+  name: "GlMap",
+
+  mixins: [withWatchers, withAsyncActions, withPrivateMethods, withEvents],
+
+  props: {
+    mapboxGl: {
+      type: Object,
+      default: null
+    },
+    ...options
+  },
+
+  provide() {
+    console.log('GlMap.vue provide is running, this.mapbox:', this.mapbox);
+    const self = this;
+    return {
+      get mapbox() {
+        console.log('GlMap.vue provide mapbox running')
+        return self.mapbox;
+      },
+      get map() {
+        console.log('GlMap.vue provide map running')
+        return self.map;
+      },
+      get actions() {
+        console.log('GlMap.vue provide actions running')
+        return self.actions;
+      }
+    };
+  },
+
+  data() {
+    return {
+      initial: true,
+      initialized: false
+    };
+  },
+
+  computed: {
+    loaded() {
+      return this.map ? this.map.loaded() : false;
+    },
+    version() {
+      return this.map ? this.map.version : null;
+    },
+    isStyleLoaded() {
+      return this.map ? this.map.isStyleLoaded() : false;
+    },
+    areTilesLoaded() {
+      return this.map ? this.map.areTilesLoaded() : false;
+    },
+    isMoving() {
+      return this.map ? this.map.isMoving() : false;
+    },
+    canvas() {
+      return this.map ? this.map.getCanvas() : null;
+    },
+    canvasContainer() {
+      return this.map ? this.map.getCanvasContainer() : null;
+    },
+    images() {
+      return this.map ? this.map.listImages() : null;
+    }
+  },
+
+  created() {
+    this.map = null;
+    this.propsIsUpdating = {};
+    this.mapboxPromise = this.mapboxGl
+      ? Promise.resolve(this.mapboxGl)
+      : import("mapbox-gl");
+  },
+
+  mounted() {
+    console.log('GlMap.vue mounted, this.mapbox:', this.mapbox);
+    this.$_loadMap().then(map => {
+      this.map = map;
+      if (this.RTLTextPluginUrl !== undefined) {
+        this.mapbox.setRTLTextPlugin(
+          this.RTLTextPluginUrl,
+          this.$_RTLTextPluginError
+        );
+      }
+      const eventNames = Object.keys(mapEvents);
+      this.$_bindMapEvents(eventNames);
+      this.$_registerAsyncActions(map);
+      this.$_bindPropsUpdateEvents();
+      this.initial = false;
+      this.initialized = true;
+      this.$emit("load", { map, component: this });
+    });
+  },
+
+  beforeDestroy() {
+    this.$nextTick(() => {
+      if (this.map) this.map.remove();
+    });
+  }
+};
+</script>
+
+<style>
+.mgl-map-wrapper {
+  height: 100%;
+  position: relative;
+  width: 100%;
+}
+
+.mgl-map-wrapper .mapboxgl-map {
+  height: 100%;
+  left: 0;
+  position: absolute;
+  top: 0;
+  width: 100%;
+}
+</style>
