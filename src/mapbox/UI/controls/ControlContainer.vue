492985eec301494865b022e1f0ec9597b3edd290
andy.rothwell <rothwell.andy@gmail.com>
2020-03-04 15:42:26 -0500

    changes to make mapbox work in viewerboard

---
+++ src/mapbox/UI/controls/ControlContainer.vue
@@ -0,0 +1,43 @@
+<template>
+  <div>
+    <slot />
+  </div>
+</template>
+
+<script>
+import controlMixin from "./controlMixin";
+
+export default {
+  name: 'containerControl',
+  mixins: [controlMixin],
+
+  props: {
+    buttonText: {
+      type: String,
+      default: 'true',
+    },
+    buttonId: {
+      type: String,
+      default: 'button-id',
+    }
+  },
+  mounted() {
+    class ControlContainer {
+
+      onAdd(map){
+        console.log('ControlContainer.vue OnAdd is running, props:', props);
+      }
+
+      onRemove(map){
+        this.container.parentNode.removeChild(this.container);
+      }
+    }
+
+    const props = Object.assign({}, this.$props);
+    this.control = new ControlContainer(props);
+    this.$_addControl();
+
+  },
+};
+
+</script>
