c5a8d63cec593d34da472be66630259df9974787
andy.rothwell <rothwell.andy@gmail.com>
2020-03-10 14:58:32 -0400

    makes icon disappear and reappear in mapbox

--- src/mapbox/MbIcon.vue
+++ src/mapbox/MbIcon.vue
@@ -10,21 +10,21 @@ export default {
     // 'attribution',
   ],
   mounted() {
-    // console.log('MbIcon.vue mounted is running');
+    const map = this.$store.state.map;
+    console.log('MbIcon.vue mounted is running, map:', map);
     // const mapboxElement = this.$mapboxElement = this.createMapboxElement();
-    // const map = this.$store.state.map.map;
 
     // REVIEW kind of hacky/not reactive?
-    // if (map) {
+    if (map) {
     //   leafletElement.addTo(map);
     //   map.attributionControl.removeAttribution('overwrite');
     //   map.attributionControl.removeAttribution('<span class="esri-attributions" style="line-height:14px; vertical-align: -3px; text-overflow:ellipsis; white-space:nowrap; overflow:hidden; display:inline-block; max-width:1385px;"></span>');
-    // }
-    // this.parentMounted();
+      this.parentMounted(map);
+    }
   },
   destroyed() {
-    // console.log('EsriTiledMapLayer destroyed is running');
-    // this.$mapboxElement._map.removeLayer(this.$mapboxElement);
+    const map = this.$store.state.map.map;
+    map.removeLayer('points');
   },
   methods: {
     createMapboxElement() {
@@ -38,52 +38,45 @@ export default {
     parentMounted(parent) {
       const map = parent.map;
       // const map = this.$store.state.map.map;
-      console.log('MbIcon.vue parentMounted is running, map:', map);//, 'map:', map);
+      console.log('MbIcon.vue parentMounted is running, map:', map);
       map.loadImage(
-        // '../assets/markers.png',
         this.$props.url,
-        // 'https://mapboard-images.s3.amazonaws.com/camera.png',
-        // 'https://upload.wikimedia.org/wikipedia/commons/thumb/6/60/Cat_silhouette.svg/400px-Cat_silhouette.svg.png',
         function(error, image) {
           if (error) throw error;
           console.log('in loadImage callback, map:', map);
-          map.addImage('markers', image);
-          map.addSource('point', {
-            'type': 'geojson',
-            'data': {
-            'type': 'FeatureCollection',
-            'features': [
-              {
-                'type': 'Feature',
-                'geometry': {
-                  'type': 'Point',
-                  'coordinates': [-75.163471, 39.953338]
-                }
+          if (!Object.keys(map.style.imageManager.images).includes('markers')) {
+            map.addImage('markers', image);
+          }
+          if (!Object.keys(map.style.sourceCaches).includes('point')) {
+            map.addSource('point', {
+              'type': 'geojson',
+              'data': {
+                'type': 'FeatureCollection',
+                'features': [
+                  {
+                    'type': 'Feature',
+                    'geometry': {
+                      'type': 'Point',
+                      'coordinates': [-75.163471, 39.953338]
+                    }
+                  }
+                ]
               }
-            ]
+            });
           }
-        });
-        map.addLayer({
-          'id': 'points',
-          'type': 'symbol',
-          'source': 'point',
-          'layout': {
-            'icon-image': 'markers',
-            'icon-size': 0.10
-          }
+          map.addLayer({
+            'id': 'points',
+            'type': 'symbol',
+            'source': 'point',
+            'layout': {
+              'icon-image': 'markers',
+              'icon-size': 0.10
+            }
           });
         }
       );
-      // map.addSource('labels', {
-      //   type: 'raster',
-      //   tiles: [this.$props.url + '/tile/{z}/{y}/{x}'],
-      //   tileSize: 256
-      // });
-      // map.addLayer(this.$mapboxElement);
     },
   },
-  // we don't actually render anything, but need to define either a template
-  // or a render function
   render(h) {
     return;
   },
