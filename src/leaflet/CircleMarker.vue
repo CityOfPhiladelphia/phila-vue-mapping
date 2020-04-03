86023b8bc24c43296d1b64c3f650fe7945bea994
Andy Rothwell <rothwell.andy@gmail.com>
2020-04-03 13:36:59 -0400

    changes to circlemarker to make clicks work

--- src/leaflet/CircleMarker.vue
+++ src/leaflet/CircleMarker.vue
@@ -24,12 +24,15 @@ export default {
       if (map) {
         leafletElement.addTo(map);
       }
+      bindEvents(this, this.$leafletElement, this._events);
     },
   },
   mounted() {
+    // console.log('CircleMarker mounted is running');
     const leafletElement = this.$leafletElement = this.createLeafletElement();
     const map = this.$store.state.map.map;
     if (map) {
+      // console.log('CircleMarker mounted if leafletElement:', leafletElement, 'map:', map);
       leafletElement.addTo(map);
     }
 
@@ -37,11 +40,16 @@ export default {
     // TODO warn if trying to bind an event that doesn't exist
     bindEvents(this, this.$leafletElement, this._events);
   },
+  // updated() {
+  //   console.log('CircleMarker updated is running');
+  // },
   destroyed() {
+    // console.log('CircleMarker.vue destroyed is running');
     this.$leafletElement._map.removeLayer(this.$leafletElement);
   },
   methods: {
     createLeafletElement() {
+      // console.log('CircleMarker.vue createLeafletElement is running');
       const props = this.$props;
       const {
         latlng,
@@ -53,6 +61,7 @@ export default {
     parentMounted(parent) {
       const map = parent.$leafletElement;
       this.$leafletElement.addTo(map);
+      // bindEvents(this, this.$leafletElement, this._events);
     },
   },
   // we don't actually render anything, but need to define either a template
